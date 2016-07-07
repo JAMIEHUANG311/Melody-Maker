7e0b2b4631ba9e639ba9f18e483b01c9ca17bcf7
JAMIEHUANG311 <jamie.huang@gmail.com>
2016-07-07 17:22:26 +0900

    Melody Maker
    Play music, change speed, pitch, volume, etc.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Melody Player
+ *
+ * This file contains the main method to run the program.
+ */
+
+import melody.gui.*;
+
+public class Main {
+	/*
+	 * Runs the program.
+	 */
+	public static void main(String[] args) {
+		new MelodyGUI();
+	}
+}
